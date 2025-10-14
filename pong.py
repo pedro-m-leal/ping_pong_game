97253fa717690ef2e3ea3809204b9492b9ff091c
João Baptista <14jbaptista@gmail.com>
2025-10-14 19:18:52 +0100

    introduction of the bars and possible movement of one of them

--- pong.py
+++ pong.py
@@ -30,19 +30,28 @@ class Player(GameSprite):
     # method to control the sprite with arrow keys
     def update(self):
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_DOWN] and self.rect.y > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+        if keys[K_UP] and self.rect.y < 495:
             self.rect.x += self.speed
+
+#create the elements of the game
+player_1 = Player(actual_pong_bar.png, (150,150))
+player_2 = Player(actual_pong_bar.png, (350,350))
 
 run = True
 clock = time.Clock()
 FPS = 60
+speed = 5
 
 while run:
+    #carachter introduction in the loop
+    player_1.update()
+    #carachter update per loop
+    player_1.reset()
     for e in event.get():
         if e.type == QUIT:
             run = False
-    
+
     display.update()
     clock.tick(FPS)
